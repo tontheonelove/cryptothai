167a8908e42632858091eb9888d284fb945f856f
tontheonelove <tonlove5233@gmail.com>
2017-11-12 12:43:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
     $json = json_decode($content, true);
    // print_R($json);
     $count=count($json);
-    echo'<th>pairing_id</th><th>change</th><th>last_price</th><th>volume_24hours</th>';
+    echo'<table><th>pairing_id</th><th>change</th><th>last_price</th><th>volume_24hours</th>';
     for($i=0;$i<$count;$i++)
     {
       echo'<tr><td>'.$json[$i]['pairing_id:1'].'</td><td>'.$json[$i]['change'].'</td><td>'.$json[$i]['last_price'].'</td><td>'.$json[$i]['volume_24hours'].'</td></tr>';
     }
   
+echo'</table>'
   ?>
