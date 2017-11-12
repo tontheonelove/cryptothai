a101d4783b9ebb21899b311011d459b5ca42dfe8
tontheonelove <tonlove5233@gmail.com>
2017-11-12 12:26:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo"></p>
+
 <?php
-echo "Hello LINE BOT";
+    $content=file_get_contents("https://bx.in.th/api/");  // add your url which contains json file
+    $json = json_decode($content, true);
+   // print_R($json);
+    $count=count($json);
+    echo'<table 100%><th>pairing_id</th><th>change</th><th>last_price</th><th>volume_24hours</th>';
+    for($i=0;$i<$count;$i++)
+    {
+      echo'<tr><td>'.$json[$i]['pairing_id'].'</td><td>'.$json[$i]['change'].'</td><td>'.$json[$i]['last_price'].'</td><td>'.$json[$i]['volume_24hours'].'</td></tr>';
+    }
+   echo'</table>';
+  ?>
+
+</body>
+</html>
