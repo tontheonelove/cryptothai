f33b6899e297df7752de1309506bb8f5de6764f9
tontheonelove <tonlove5233@gmail.com>
2017-11-14 10:20:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,25 +17,19 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			if($text=='BCH'){
-				$json=json_decode(file_get_contents("https://api.coinmarketcap.com/v1/ticker/bitcoin-cash/?convert=THB"),true);
-				$count=count($json);
-				$text='';
-				for($i=0;$i<$count;$i++){
-				$text=$text+'เหรียญ : '.$json[$i]['name']."\nขึ้น-ลง : ".$json[$i]['percent_change_24h']."\nราคาล่าสุด :".$json[$i]['price_thb']."\n";
-				}
-				
-			if($text=='BTC'){
-				$json=json_decode(file_get_contents("https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=THB"),true);
-				$count=count($json);
-				$text='';
-				for($i=0;$i<$count;$i++){
-				$text=$text+'เหรียญ : '.$json[$i]['name']."\nขึ้น-ลง : ".$json[$i]['percent_change_24h']."\nราคาล่าสุด :".$json[$i]['price_thb']."\n";
-				}
-				
-				
-	
+			$list = {
+				'BCH':'bitcoin-cash',
+				'BTC':'bitcoin',
 			}
+			if($list[$text]){
+				$json=json_decode(file_get_contents("https://api.coinmarketcap.com/v1/ticker/".$list[$text]."/?convert=THB"),true);
+				$count=count($json);
+				$text='';
+				for($i=0;$i<$count;$i++){
+					$text=$text+'เหรียญ : '.$json[$i]['name']."\nขึ้น-ลง : ".$json[$i]['percent_change_24h']."\nราคาล่าสุด :".$json[$i]['price_thb']."\n";
+				}
+			}
+				
 			$messages = [
 				'type' => 'text',
 				'text' => $text
