5fc60ff4d6c27bf963716ce09ab4d1f85558a6ba
tontheonelove <tonlove5233@gmail.com>
2017-11-14 10:07:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if (!is_null($events['events'])) {
 
 				$count=count($json);
 				$text='';
-				for($i='';$i<$count;$i++){
+				for($i=0;$i<$count;$i++){
 				$text=$text+'เหรียญ : '.$json[$i]['name']."\nขึ้น-ลง : ".$json[$i]['percent_change_24h']."\nราคาล่าสุด :".$json[$i]['price_thb']."\n";
 				}
 			}
